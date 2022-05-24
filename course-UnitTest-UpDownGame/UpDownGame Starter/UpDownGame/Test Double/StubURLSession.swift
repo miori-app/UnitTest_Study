7affdefcb0835f19bbd554d4e531d61a9eaee0df
Mi Yeon Lee <immioriii@gmail.com>
2022-05-24 23:49:33 +0900

    test : 의존성 주입 연습

--- course-UnitTest-UpDownGame/UpDownGame Starter/UpDownGame/Test Double/StubURLSession.swift
+++ course-UnitTest-UpDownGame/UpDownGame Starter/UpDownGame/Test Double/StubURLSession.swift
@@ -5,6 +5,7 @@
 
 import Foundation
 
+//MARK: URLSession이 네트워킹을 통해 받아와서 처리하는 data, response, error를 직접 만들어서 담아주기 위한 구조체입니
 struct DummyData {
     let data: Data?
     let response: URLResponse?
@@ -16,6 +17,7 @@ struct DummyData {
     }
 }
 
+//MARK: 테스트 중에 URLSession을 대체할 객체
 class StubURLSession: URLSessionProtocol {
     var dummyData: DummyData?
     
@@ -28,6 +30,7 @@ class StubURLSession: URLSessionProtocol {
     }
 }
 
+//MARK: URLSessionDataTask를 상속받은 객체로 테스트 중에 URLSessionDataTask를 대체할 객체
 class StubURLSessionDataTask: URLSessionDataTask {
     var dummyData: DummyData?
     
--- course-UnitTest-UpDownGame/UpDownGame Starter/UpDownGame/Test Double/URLSessionProtocol.swift
+++ course-UnitTest-UpDownGame/UpDownGame Starter/UpDownGame/Test Double/URLSessionProtocol.swift
@@ -7,6 +7,7 @@ import Foundation
 
 typealias DataTaskCompletionHandler = (Data?, URLResponse?, Error?) -> Void
 
+// MARK: 실제 URLSession 대신 사용할 테스트를 위한 URLSession 객체를 만들어 주기 위한 프로토콜
 protocol URLSessionProtocol {
     func dataTask(with url: URL,  completionHandler: @escaping DataTaskCompletionHandler) -> URLSessionDataTask
 }
